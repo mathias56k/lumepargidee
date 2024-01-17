caa143c28c533ea753f4ae91aed4ca2c814dcb65
Mathias Talo <mathias.talo@outlook.com>
2024-01-18 01:18:23 +0200

    Added checking for open or not

--- src/AppTest.jsx
+++ src/AppTest.jsx
@@ -26,18 +26,45 @@ const AppTest = () => {
     if (!placesData) {
       return <div>Loading...</div>;
     }
+
+    const time = new Date()
   
     return (
       <div>
         {Object.keys(placesData).map((key) => {
           const restaurant = placesData[key];
           const todayOpeningHours = restaurant[Object.keys(restaurant)[currentDay]];
-  
+          const currentTime = time.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+    
+          const openTime = todayOpeningHours.open !== 'null' ? todayOpeningHours.open.split(':') : null;
+          const closeTime = todayOpeningHours.close !== 'null' ? todayOpeningHours.close.split(':') : null;
+
+          const openingDate = openTime ? new Date().setHours(Number(openTime[0]), Number(openTime[1]), 0, 0) : null;
+          const closingDate = closeTime ? new Date().setHours(Number(closeTime[0]), Number(closeTime[1]), 0, 0) : null;
+
+          const isOpenNow = openingDate && closingDate && time.getTime() >= openingDate && time.getTime() <= closingDate;
+
+          const nextOpeningDate = new Date();
+          nextOpeningDate.setDate(nextOpeningDate.getDate() + 1);
+          nextOpeningDate.setHours(openTime[0], openTime[1], 0, 0);
+
           return (
             <div className="flex justify-center mt-8 gap-8 w-full flex-wrap" key={key}>
-              <div className="w-[90%] max-w-[30rem] h-[18rem] bg-[#98b9e5] rounded-xl flex flex-col justify-between" key={key}>
-                <div className="w-full bg-[#134a91] h-[20%] rounded-t-xl flex justify-center items-center">
-                  <p className="font-extrabold text-xl text-[#98b9e5]">AVATUD</p>
+              <div className="w-[90%] max-w-[30rem] h-[18rem] bg-[#98b9e5] rounded-xl flex flex-col" key={key}>
+                <div className="w-full bg-[#134a91] h-[25%] rounded-t-xl flex justify-center items-center">
+                <div className="w-full flex justify-center items-center gap-1 mb-2">
+                  {isOpenNow ? (
+                    <div className='flex flex-col items-center h-full'>
+                      <p className="font-bold text-[1.5rem] text-[#98b9e5]">AVATUD</p>
+                      <p className='text-[#98b9e5] mt-[-0.25rem]'>Suletakse {todayOpeningHours.close}</p>
+                    </div>
+                  ) : (
+                <div className='flex flex-col items-center h-full'>
+                  <p className="font-bold text-[1.5rem] h-[70%] text-[#CF3438]">SULETUD</p>
+                  <p className='text-[#98b9e5] mt-[-0.25rem]'>Avatakse homme {nextOpeningDate.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}</p>
+                </div>
+              )}
+            </div>
                 </div>
                 <div className="mx-3 flex items-center justify-between text-[#0b121b]">
                   <div>
@@ -49,13 +76,6 @@ const AppTest = () => {
                     <img className="h-full" src="/location.svg" alt="" />
                     <p>15 km</p>
                   </div>
-                </div>
-                <div className="w-full flex justify-center items-center h-[15%] gap-1 mb-2">
-                  {todayOpeningHours.open !== 'null' && todayOpeningHours.close !== 'null' ? (
-                    <p className="font-bold text-[1.5rem] text-[#134a91]">Open until {todayOpeningHours.close}</p>
-                  ) : (
-                    <p className="font-bold text-[1.5rem] text-[#ff0000]">Closed today</p>
-                  )}
                 </div>
               </div>
             </div>
